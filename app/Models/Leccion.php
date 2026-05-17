eadbc33ccbcd16556948b86e6042b5b5a6ba51a8
MarlenyMendez <marlenymendez@gmail.com>
2026-05-17 16:15:46 -0600

    Limpiados conflictos en modelos

--- app/Models/Leccion.php
+++ app/Models/Leccion.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,35 +9,11 @@ class Leccion extends Model
 {
     use HasFactory;
 
-    // Especificar el nombre de la tabla (importante para español)
     protected $table = 'lecciones';
-=======
-use Illuminate\Database\Eloquent\Model;
-
-
- // Representa una lección dentro de un módulo.
- 
-class Leccion extends Model
-{
-    protected $table = 'lecciones';
-    public $timestamps = false;
->>>>>>> 8074006363a4f89c4d9d6e456069e7498cf1da13
-
     protected $fillable = ['modulo_id', 'titulo', 'contenido', 'orden'];
 
     public function modulo()
     {
-<<<<<<< HEAD
         return $this->belongsTo(Modulo::class);
     }
 }
-=======
-        return $this->belongsTo(Modulo::class, 'modulo_id');
-    }
-
-    public function progreso()
-    {
-        return $this->hasMany(ProgresoUsuario::class, 'leccion_id');
-    }
-}
->>>>>>> 8074006363a4f89c4d9d6e456069e7498cf1da13
